6f85a73dde8b780de8019cac190bee75e3329844
koalakid1 <koalakid154@gmail.com>
2020-03-12 18:13:41 +0900

    200312

--- C/baekjoon C/function/1065.c
+++ C/baekjoon C/function/1065.c
@@ -1,17 +1,35 @@
 //#include<stdio.h>
 //#include<string.h>
 //
+//int checking(int a, int b) {
+//
+//	int c = a / 100, d = (a / 10) % 10, e = a % 10;
+//
+//	if ((c - d) == (d - e))
+//		return b + 1;
+//
+//	else return b;
+//
+//}
+//
 //int main() {
 //
-//	int num;
-//	scanf_s("%d", &num);
+//	int n;
+//	scanf_s("%d", &n);
 //
-//	if (num < 100) printf("%d", num);
+//	if (n < 100) printf("%d",n);
 //
 //	else {
 //
+//		int count = 99; 
 //
+//		for(int i = 100; i<=n; i++)
+//			if (i < 1000) {
 //
+//				count = checking(i, count);
+//
+//			}
+//		printf("%d", count);
 //	}
 //
 //}
